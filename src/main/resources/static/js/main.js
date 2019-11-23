7fd8a4c69387c38acc413d80d09fd7d3c9c12311
Yuriy <klyuyko.yu@gmail.com>
2019-11-23 18:23:13 +0200

    add index.html & static resources

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+    $('#datetimepicker3').datetimepicker({
+        format: 'LT'
+    });
+});
+
+$(function () {
+    $('#datetimepicker4').datetimepicker({
+        format: 'L'
+    });
+});
+
+$('#myModal').on('show.bs.modal', function (e) {
+    console.log(this);
+    let stations = $(e.relatedTarget).data('stations');
+    console.log(stations);
+    $(this).find('.modal-body input').val(stations);
+});
+
+$(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });
+});
+
+$('.example-popover').popover({
+    trigger: 'focus'
+});

e4df2e63bc7964e20a8922807b3e3e00977bcd61
Yuriy <klyuyko.yu@gmail.com>
2019-11-23 18:26:14 +0200

    add empty RouteRepository definition

--- src/main/java/com/proky/booking/RailwayTicketBookingApplication.java
+++ src/main/java/com/proky/booking/RailwayTicketBookingApplication.java
@@ -1,13 +1,30 @@
 package com.proky.booking;
 
+import com.proky.booking.persistence.entities.User;
+import com.proky.booking.persistence.repository.UserRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
+@Log4j2
+//public class RailwayTicketBookingApplication implements CommandLineRunner {
 public class RailwayTicketBookingApplication {
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RailwayTicketBookingApplication.class, args);
 	}
 
+//	@Override
+//	public void run(String... args) throws Exception {
+//		final Optional<User> byId = userRepository.findById(1L);
+//		final User user = byId.orElse(new User());
+//		log.info("user, {}", user);
+//	}
 }
--- src/main/java/com/proky/booking/persistence/repository/RouteRepository.java
+++ src/main/java/com/proky/booking/persistence/repository/RouteRepository.java
@@ -2,6 +2,8 @@ package com.proky.booking.persistence.repository;
 
 import com.proky.booking.persistence.entities.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface RouteRepository extends JpaRepository<Route, Long> {
+
 }

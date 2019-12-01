518c48fb9227ccde7cf7abfee93a1af92406988f
Yuriy <klyuyko.yu@gmail.com>
2019-12-01 16:48:03 +0200

    add CustomErrorController

---
+++ src/main/java/com/proky/booking/presentation/controller/CustomErrorController.java
@@ -0,0 +1,42 @@
+package com.proky.booking.presentation.controller;
+
+import com.proky.booking.util.properties.ViewPath;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Log4j2
+@Controller
+@AllArgsConstructor
+public class CustomErrorController implements ErrorController {
+    private ViewPath viewPath;
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+
+        String errorViewPath = viewPath.errorRuntime;
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                errorViewPath =  viewPath.errorNotFound;
+            }
+        }
+
+        return errorViewPath;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return viewPath.errorRuntime;
+    }
+
+}

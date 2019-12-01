6bec2809f6120765cacebd3b92ec10d271f39bf5
Yuriy <klyuyko.yu@gmail.com>
2019-12-02 01:18:21 +0200

    refactor LanguageController

--- src/main/java/com/proky/booking/presentation/controller/LanguageController.java
+++ src/main/java/com/proky/booking/presentation/controller/LanguageController.java
@@ -1,21 +1,22 @@
 package com.proky.booking.presentation.controller;
 
+import com.proky.booking.util.constans.http.Attributes;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @Controller
+@SessionAttributes(Attributes.LANGUAGE)
 public class LanguageController {
-
     @GetMapping("/changeLanguage")
-    public String chnageLanguage(@RequestParam(name="language", required=false, defaultValue="en") String language, Model model) {
-        log.info("language is {}", language);
-
-        return "index";
+    public String changeLanguage(@RequestParam(defaultValue = "en") String language,
+                                 @RequestHeader(value = "referer", required = false) final String referer,
+                                 Model model) {
+        log.info("changing language, selected lang is: {}", language);
+        log.info("referer is {}", referer);
+        model.addAttribute(Attributes.LANGUAGE, language);
+        return "redirect:" + referer;
     }
 }

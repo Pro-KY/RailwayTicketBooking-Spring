154a2ccc89c13234c8ba718ffc2ef0948ef34043
Yuriy <klyuyko.yu@gmail.com>
2019-12-03 18:16:19 +0200

    refactor security Services

--- src/main/java/com/proky/booking/service/security/SecurityService.java
+++ src/main/java/com/proky/booking/service/security/SecurityService.java
@@ -1,5 +1,6 @@
 package com.proky.booking.service.security;
 
+import com.proky.booking.dto.SecureUserDto;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,15 +10,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityService {
+
     public boolean isAnonymousUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return  authentication instanceof AnonymousAuthenticationToken;
+        return getAuthentication() instanceof AnonymousAuthenticationToken;
     }
 
     public String getUserRole() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        UserDetails userDetails = (UserDetails) getAuthentication().getPrincipal();
         final GrantedAuthority next = userDetails.getAuthorities().iterator().next();
         return next.getAuthority();
     }
+
+    public SecureUserDto getCurrentUser() {
+        return (SecureUserDto) getAuthentication().getPrincipal();
+    }
+
+    private Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
 }
--- src/main/java/com/proky/booking/service/security/UserDetailsServiceImpl.java
+++ src/main/java/com/proky/booking/service/security/UserDetailsServiceImpl.java
@@ -1,11 +1,10 @@
 package com.proky.booking.service.security;
 
-import com.proky.booking.persistence.entities.Role;
+import com.proky.booking.dto.SecureUserDto;
 import com.proky.booking.persistence.entities.User;
 import com.proky.booking.persistence.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 @Log4j2
@@ -30,12 +28,21 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         log.info("loadUserByUsername");
 
         final User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+        final Set<GrantedAuthority> grantedAuthorities = buildUserAuthority(user);
+        return buildUserForAuthentication(user, grantedAuthorities);
+    }
 
-        final String userName = user.getFirstName().concat(" ").concat(user.getLastName());
-//        final String userName = user.getEmail();
+    private Set<GrantedAuthority> buildUserAuthority(User user) {
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().getName()));
+        return grantedAuthorities;
+    }
 
-        return new org.springframework.security.core.userdetails.User(userName, user.getPassword(), grantedAuthorities);
+    private org.springframework.security.core.userdetails.User buildUserForAuthentication(User user, Set<GrantedAuthority> authorities) {
+        final String userName = user.getFirstName().concat(" ").concat(user.getLastName());
+        String password = user.getPassword();
+        final SecureUserDto secureUserDto = new SecureUserDto(userName, password, true, true, true, true, authorities);
+        secureUserDto.setUserId(user.getId());
+        return secureUserDto;
     }
 }
